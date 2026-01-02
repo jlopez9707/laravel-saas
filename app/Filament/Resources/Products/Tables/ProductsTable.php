99a76322f53902319426f45c412df4e71a3fc839
javi9707 <javierlopezrequejo@gmail.com>
2026-01-02 17:39:01 -0500

    Add category filter to products table and update translations
    Introduced a SelectFilter for categories in the products table to allow filtering by category. Updated English and Spanish translation files to include new keys for 'Categories', 'No products found', and 'You have not created any products yet'.

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -2,13 +2,14 @@
 
 namespace App\Filament\Resources\Products\Tables;
 
+use App\Models\Category;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
-
 class ProductsTable
 {
     public static function configure(Table $table): Table
@@ -30,14 +31,16 @@ class ProductsTable
                     ->sortable(),
             ])
             ->filters([
-                //
+                SelectFilter::make('category_id')
+                ->label(__('Categories'))
+                ->options(fn (): array => Category::query()->pluck('name', 'id')->all())
             ])
             ->recordActions([
                 EditAction::make(),
                 DeleteAction::make(),
             ])
             ->emptyStateHeading(__('No products found'))
-            ->emptyStateDescription(__('You have not created any products yet.'))
+            ->emptyStateDescription(__('You have not created any products yet'))
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
